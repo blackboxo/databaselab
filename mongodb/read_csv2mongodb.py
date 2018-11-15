a983c5e8fc517c28491381e70d16e9ecd6ae4175
wangfangye <786632563@qq.com>
2018-11-15 15:22:41 +0800

    mongodb读取CSV数据到数据库

---
+++ mongodb/read_csv2mongodb.py
@@ -0,0 +1,36 @@
+# -*- coding: UTF-8 -*-
+'''
+@project:databaselab2
+@author:wangfy
+@time:2018/11/12 18:02
+'''
+
+import pymongo
+import csv
+import os
+import datetime
+from collection_factory import CollectionFactory
+from pymongo import InsertOne
+
+def read_csv2mongodb(filename,collectionName):
+    mydb = CollectionFactory.create_client_and_db()
+    # filename = "test2.csv"
+    mydb[collectionName].delete_many({})
+    count = 0
+    with open(filename, encoding='utf-8') as fr:
+        rows = csv.DictReader(fr)
+        post_list = []
+        # print(type(rows))
+        for row in rows:
+            post_list.append(row)
+            count += 1
+        mydb[collectionName].bulk_write(list(map(InsertOne, post_list)))
+    return count
+
+if __name__ == '__main__':
+    fileName = "‪test2.csv"
+    collectionName = "test_posts"
+    count = read_csv2mongodb(fileName,collectionName)
+    print(count)
+
+
