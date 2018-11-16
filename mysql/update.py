2f4fb74d92506397789c6fd13fd5fbd31aefcc05
blackboxo <592276898@qq.com>
2018-11-16 14:34:12 +0800

    fix sql

--- mysql/update.py
+++ mysql/update.py
@@ -162,7 +162,7 @@ def update_aggregate(num, average_iteration_num=1):
         #     })
         # session.commit()
         conn = session.connect()
-        sql = 'UPDATE users,posts INNER JOIN posts ON users.id = posts.OwnerUserId SET users.reputation = users.Reputation + 1,posts.ViewCount = posts.ViewCount+1 WHERE posts.ViewCount > {num}'.format(
+        sql = 'UPDATE users INNER JOIN posts ON users.id = posts.OwnerUserId SET users.reputation = users.Reputation + 1,posts.ViewCount = posts.ViewCount+1 WHERE posts.ViewCount > {num}'.format(
             num=num)
         s = text(sql)
         res = conn.execute(s)
@@ -188,7 +188,7 @@ def update_aggregate(num, average_iteration_num=1):
         #     })
         # session.commit()
         conn = session.connect()
-        sql = 'UPDATE users,posts INNER JOIN posts ON users.id = posts.OwnerUserId SET users.reputation = users.Reputation - 1,posts.ViewCount = posts.ViewCount-1 WHERE posts.ViewCount > {num}'.format(
+        sql = 'UPDATE users INNER JOIN posts ON users.id = posts.OwnerUserId SET users.reputation = users.Reputation - 1,posts.ViewCount = posts.ViewCount-1 WHERE posts.ViewCount > {num}'.format(
             num=num)
         s = text(sql)
         res = conn.execute(s)
