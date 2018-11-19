d76d3970b09cc9774b81347ca5e4951c7f4ef040
DESKTOP-7LSA2S8\lmw <lmwtclmwtc@163.com>
2018-11-19 11:59:34 +0800

    change the import problem

--- mysql_experiment_all.py
+++ mysql_experiment_all.py
@@ -6,14 +6,14 @@ from mysql.update import start_test_update_and_record_result
 
 if __name__ == "__main__":
     ## 这个脚本负责批量进行所有测试
-    start_test_delete_filter_and_record_result(
-        start_test_num=10000, max_test_num=100000, step=10000, iteration_num=3)
-
     start_test_insert_and_delete_and_record_result(
         start_test_num=10000, max_test_num=100000, step=10000, iteration_num=3)
 
-    start_test_search_and_record_result(
+    start_test_delete_filter_and_record_result(
         start_test_num=10000, max_test_num=100000, step=10000, iteration_num=3)
 
     start_test_update_and_record_result(
+        start_test_num=10000, max_test_num=100000, step=10000, iteration_num=3)
+
+    start_test_search_and_record_result(
         start_test_num=10000, max_test_num=100000, step=10000, iteration_num=3)
